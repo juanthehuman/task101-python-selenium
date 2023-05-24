4cf4cb694a3f19fb2de630b707191e9742042149
Juan <juan8.devmode@gmail.com>
2023-05-24 19:54:07 +0800

    Added .gitignore, requirements.txt

---
+++ README.md
@@ -0,0 +1,40 @@
+Data Scraping with Python and Selenium
+This repository contains a Python script that enables web scraping automation for retrieving the top 10 "People also ask" results from Google search results.
+
+Getting Started
+To use the script, follow the steps below:
+
+Clone this repository to your local machine.
+
+Activate a virtual environment to isolate your project dependencies.
+
+Install the required dependencies by running the following command in your terminal or command prompt:
+pip install -r requirements.txt
+
+Run the script by executing the following command in your terminal or command prompt:
+python script.py
+
+When prompted, enter the desired keyword and press enter.
+
+Wait for the automation process to complete.
+
+The results will be displayed in the terminal as a Python dictionary.
+
+Requirements
+Make sure you have the following dependencies installed:
+
+Python 3.x
+Selenium library
+You can install the Selenium library and other dependencies by running the command mentioned in the "Getting Started" section.
+
+Please note that the script utilizes Selenium, so ensure that you have the appropriate web driver installed for the browser of your choice (e.g., ChromeDriver for Google Chrome).
+
+Usage
+Once you have completed the setup steps and installed the necessary dependencies, you can run the script to automate the scraping process for the top 10 "People also ask" results on Google.
+
+Feel free to customize the script or integrate it into your own projects as needed.
+
+Acknowledgments
+We would like to express our gratitude to the developers and contributors of Selenium for providing a powerful tool for web scraping and automation.
+
+If you have any questions or suggestions, please feel free to reach out.
--- script.py
+++ script.py
@@ -4,13 +4,14 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
+from chromedriver_binary import chromedriver_filename
 import time
 
 website = "https://www.google.com/"
-path = "/usr/bin/chromedriver"
+webdriver_path = chromedriver_filename
 
 try:
-    service = Service(executable_path=path)
+    service = Service(executable_path=webdriver_path)
     driver = webdriver.Chrome(service=service)
 
     # Prompt the user
@@ -21,12 +22,19 @@ try:
             # Navigate to the Google search page
             driver.get(website)
 
-            search_input = driver.find_element(by="xpath", value='//textarea[@class="gLFyf"]/.')
+            search_input = driver.find_element(by="xpath", value='//textarea[@id="APjFqb"]')
             search_input.send_keys(kw)
             search_input.send_keys(Keys.RETURN)
 
-            container_elements = driver.find_elements(by="xpath", value='//div[@jsname="YrZdPb"]/.')
-            nav_element = driver.find_element(by="xpath", value='//div[@class="CvDJxb"]/.')
+            # Scroll to People also ask
+            paa_element = driver.find_element(by="xpath", value='//div[@class="T6zPgb"]')
+            driver.execute_script("arguments[0].scrollIntoView(true);", paa_element)
+
+            # Change nav element style position fixed to absolute
+            nav_element = driver.find_element(by="xpath", value='//div[@id="searchform"]')
+            driver.execute_script("arguments[0].style.position = 'absolute';", nav_element)
+
+            container_elements = driver.find_elements(by="xpath", value='//div[@jsname="YrZdPb"]')
 
             faqs = {}
             limit = 10
@@ -34,32 +42,31 @@ try:
             # First iteration
             for container_element in container_elements:
                 driver.execute_script("arguments[0].scrollIntoView(true);", container_element)
-                driver.execute_script("arguments[0].style.position = 'absolute';", nav_element)
 
                 wait = WebDriverWait(driver, 10)
 
-                overlay_locator = '//div[@jsname="YrZdPb"]/div[2]/div[@class="dnXCYb"]'
+                overlay_locator = '//div[@jsname="YrZdPb"]/div[2]/div[@jsname="tJHJj"]'
                 overlay = wait.until(EC.visibility_of_element_located((By.XPATH, overlay_locator)))
                 driver.execute_script("arguments[0].remove();", overlay)
 
-                element_locator = '//div[@jsname="YrZdPb"]/div[2]/div[@class="dnXCYb"]'
+                element_locator = '//div[@jsname="YrZdPb"]/div[2]/div[@jsname="tJHJj"]'
                 element = wait.until(EC.element_to_be_clickable((By.XPATH, element_locator)))
                 driver.execute_script("arguments[0].scrollIntoView(true);", element)
                 driver.execute_script("arguments[0].style.position = 'absolute';", nav_element)
                 time.sleep(5)
                 element.click()
 
-            question_elements = driver.find_elements(by="xpath", value='//div[@class="JlqpRe"]/.')
-            answer_elements = driver.find_elements(by="xpath", value='//div[@class="wDYxhc"]/.')
+            question_elements = driver.find_elements(by="xpath", value='//div[@jsname="lN6iy"]')
+            answer_elements = driver.find_elements(by="xpath", value='//div[@class="wDYxhc"]')
 
             # Final iteration
             for index, (question_element, answer_element) in enumerate(zip(question_elements, answer_elements)):
                 if index >= limit:
                     break
 
-                question = question_element.find_element(by="xpath", value='./span[1]/span[1]/.').text
+                question = question_element.find_element(by="xpath", value='./span[1]/span[1]').text
 
-                block = driver.find_element(by="xpath", value='//div[@class="bCOlv"]/.')
+                block = driver.find_element(by="xpath", value='//div[@jsname="NRdf4c"]')
                 display_property = block.value_of_css_property("display")
 
                 wait = WebDriverWait(driver, 10)
@@ -67,8 +74,9 @@ try:
                 if display_property == "block":
                     el = answer_element.find_element(by="xpath", value='./div[1]/*')
                     answer = el.text
-                else:
-                    container_locator = '//div[@jsname="YrZdPb"]/div[2]/div[@class="dnXCYb"]'
+
+                if display_property == "none": 
+                    container_locator = '//div[@jsname="YrZdPb"]/div[2]/div[@jsname="tJHJj"]'
                     container = wait.until(EC.element_to_be_clickable((By.XPATH, container_locator)))
                     time.sleep(5)
                     container.click()
